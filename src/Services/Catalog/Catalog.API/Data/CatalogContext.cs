06373b3937a477a4715d8bae42e0d4d8fc50cf4d
Hamza <hamza.taoujouti@sofrecom.com>
2022-12-05 14:22:00 +0100

    class constructor that is injected by an Iconfiguration which contains the information about db connexion to connect to db and get the data from collection

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,17 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DataBaseSetting:CnxString"));
+            var Database = client.GetDatabase(configuration.GetValue<string>("DataBaseSetting:Databse"));
+            Products = Database.GetCollection<Product>(configuration.GetValue<string>("DataBaseSetting:Collection"));
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
