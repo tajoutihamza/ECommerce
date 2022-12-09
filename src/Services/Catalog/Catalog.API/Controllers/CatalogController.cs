3d6a7a649ba797696297e2f810a82cb4f3d03b59
Hamza <hamza.taoujouti@sofrecom.com>
2022-12-09 11:13:19 +0100

    add crud to the controller

--- src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
+++ src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -4,6 +4,8 @@ using DnsClient.Internal;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Net;
@@ -46,20 +48,56 @@ namespace Catalog.API.Controllers
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Product))]
         public async Task<ActionResult<IEnumerable<Product>>> GetProductByCategory(string category)
         {
-            return Ok(await _repository.GetProductByCategory(category));
+            var products = await _repository.GetProductByCategory(category);
+            if (products == null)
+            {
+                _logger.LogError($"Products with category: {category} not found.");
+                return NotFound(products);
+            }
+            return Ok(products);
         }
         [HttpGet]
         [Route("[action]/{name}", Name = "GetProductByName")]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Product))]
         public async Task<ActionResult<IEnumerable<Product>>> GetProductByName(string name)
         {
-            return Ok(await _repository.GetProductByName(name));
+            var products = await _repository.GetProductByName(name);
+            if (products == null)
+            {
+                _logger.LogError($"Products with name: {name} not found.");
+                return NotFound(products);
+            }
+            return Ok(products);
+        }
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Product))]
+        public async Task<ActionResult<Product>> CreateProduct([FromBody] Product product)
+        {
+            await _repository.CreateProduct(product);
+            return CreatedAtRoute(nameof(GetProductById), new { id = product.Id }, product);
+        }
+        [HttpDelete("{id:length(24)}", Name = "DeleteProduct")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Product))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<Product>> DeleteProduct(string id)
+        {
+            if (await _repository.GetProduct(id) != null) 
+                return Ok(await _repository.DeleteProduct(id));
+
+            _logger.LogError($"Product with id: {id}, not found.");
+            return NotFound();
         }
 
-        
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Product))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<Product>> UpdateProduct([FromBody] Product product)
+        {
+            if (await _repository.GetProduct(product.Id) != null)
+                return Ok(await _repository.UpdateProduct(product));
 
-
-
-
+            _logger.LogError($"Product with id: {product.Id}, not found.");
+            return NotFound();
+        }
     }
 }
