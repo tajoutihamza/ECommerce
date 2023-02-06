317c09a363b8e280af8e64c0739a4d23d5f3a25b
Hamza <hamza.taoujouti@sofrecom.com>
2023-02-06 17:05:03 +0100

    add repository pattern to basket services

---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,39 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repositories.Interface;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _redisCache;
+        public BasketRepository(IDistributedCache cache)
+        {
+            _redisCache= cache ?? throw new ArgumentNullException(nameof(cache));
+        }
+        public async Task deleteBasket(string username)
+        {
+            await _redisCache.RemoveAsync(username);
+        }
+
+        public async Task<ShoppingCart> GetBasket(string username)
+        {
+            var basket = await _redisCache.GetStringAsync(username);
+            if(String.IsNullOrEmpty(basket))
+            {
+                    return null;
+            }
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart cart)
+        {
+            await _redisCache.SetStringAsync(cart.UserName,JsonConvert.SerializeObject(cart));
+            return await GetBasket(cart.UserName);
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.API/Repositories/Interface/IBasketRepository.cs
@@ -0,0 +1,12 @@
+﻿using Basket.API.Entities;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories.Interface
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> UpdateBasket(ShoppingCart cart);
+        Task<ShoppingCart> GetBasket(string username);
+        Task deleteBasket(string username);
+    }
+}
